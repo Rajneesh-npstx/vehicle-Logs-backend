758147894d32085d99285e4bf2c9b4d93fbe9ae1
Rajneesh-npstx <rajneesh.singh@npstx.com>
2026-08-19 12:46:34 +0530

    Feat: initialize Vehicle JPA entity

--- src/main/java/com/autocare/servicelog/entity/Vehicle.java
+++ src/main/java/com/autocare/servicelog/entity/Vehicle.java
@@ -1,4 +1,11 @@
 package com.autocare.servicelog.entity;
 
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "vehicles")
 public class Vehicle {
 }
