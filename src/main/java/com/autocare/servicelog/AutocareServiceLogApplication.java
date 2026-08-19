8daf54bc77b500c424cbbcce094d005cb965a5b4
Rajneesh-npstx <rajneesh.singh@npstx.com>
2026-08-19 11:50:21 +0530

    Initialize spring boot backend

---
+++ src/main/java/com/autocare/servicelog/AutocareServiceLogApplication.java
@@ -0,0 +1,13 @@
+package com.autocare.servicelog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutocareServiceLogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutocareServiceLogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/autocare/servicelog/AutocareServiceLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.autocare.servicelog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutocareServiceLogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
