c1ae93ecb25c00d894e78f35c2ff4c5a830cf18c
rajneesh-npstx <rajneesh.singh@npstx.com>
2026-08-19 11:20:03 +0530

    initialize Spring Boot backend

---
+++ src/main/java/com/autocare/servicelog/VehicleServiceLogBackendApplication.java
@@ -0,0 +1,13 @@
+package com.autocare.servicelog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleServiceLogBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VehicleServiceLogBackendApplication.class, args);
+    }
+
+}
