87056c54db396ddf6d7df1f9c8e586ba8d2429f6
rajneesh-npstx <rajneesh.singh@npstx.com>
2026-08-19 11:23:14 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# vehicle-Logs-backend
---
+++ src/test/java/com/autocare/servicelog/VehicleServiceLogBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.npst.vehicleservicelogbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleServiceLogBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
